444dd7153f77c6b130c9cd33816d22a7cb35ca20
SyedFarhanAli19 <exoriousyt2250@gmail.com>
2024-12-02 21:34:54 +0530

    completed this solution

--- TransposeMatrix.cpp
+++ TransposeMatrix.cpp
@@ -4,6 +4,41 @@ using namespace std;
 
 int main()
 {
+    int row ,column;
+    cout << "Enter row and column" << endl;
+    cin >> row >> column;
+    int array[row][column];
+    //input 
+    cout << "Enter elements of array" << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            cin >> array[i][j];
+        }
+        
+    }
+    
+    //processing
+    int transpose[column][row];
+    for (int i = 0; i < column; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            transpose[i][j] = array[j][i];
+        }
+        
+    }
+    
+    //output section
+    for (int i = 0; i < column; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            cout << transpose[i][j] << " ";
+        }
+        cout << endl;
+    }
     
     return 0;
 }
