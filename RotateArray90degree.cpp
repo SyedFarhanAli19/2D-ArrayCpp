727b4c8ddd7fa44448e7822452bc4b827ed4d992
SyedFarhanAli19 <exoriousyt2250@gmail.com>
2024-12-02 22:04:10 +0530

    completed this  solution

--- RotateArray90degree.cpp
+++ RotateArray90degree.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 //given a square matrix, turn it by 90 degrees in a clockwise direction
 //without using any extra space
@@ -7,7 +8,21 @@ void rotateArray(vector<vector<int>>&vec)
 {
     int size = vec.size();
 
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            swap(vec[i][j],vec[j][i]);
+        }
+        
+    }
     
+    for (int i = 0; i < size; i++)
+    {
+        reverse(vec[i].begin(),vec[i].end());
+    }
+    
+
 }
 int main()
 {
