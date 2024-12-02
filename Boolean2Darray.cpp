65cb5355bddc2ed76c8e1b6b11a0a406826626ce
SyedFarhanAli19 <exoriousyt2250@gmail.com>
2024-12-02 18:56:51 +0530

    finished solution

--- Boolean2Darray.cpp
+++ Boolean2Darray.cpp
@@ -4,6 +4,31 @@
 using namespace std;
 //given a boolean 2d array, where each row is sorted.
 //find the row with the maximun number of 1s.
+//35:50
+int leftMostOneRow(vector<vector<int>> &vec)
+{
+    int leftMostOne = -1;
+    int maxOnesRow = -1;
+    int j = vec[0].size() - 1;
+
+    while (j >= 0 && vec[0][j] == 1)
+    {
+        leftMostOne = j;
+        maxOnesRow = 0;
+        j--;
+    }
+    for (int i = 1; i < vec.size(); i++)
+    {
+        while (j >= 0 && vec[i][j] == 1)
+        {
+            leftMostOne = j;
+            j--;
+            maxOnesRow = i;    
+        }
+        
+    }
+    return maxOnesRow;
+}
 
 int maximumOnesRow(vector<vector<int>> &vec1)
 {
